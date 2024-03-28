095f4efa10c257c70eb06221794a308f8db89f16
Jonathan Smith <jhsmith0528@gmail.com>
2024-03-28 11:10:43 -0500

    PP rotation target degrees instead of radians.

--- src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -153,7 +153,7 @@ public class CommandSwerveDrivetrain extends SwerveDrivetrain implements Subsyst
   // drivetrain.setChassisSpeeds(HOLO_CONTROLLER.calculate(current, new
   // Pose2d(current.getTranslation(), new Rotation2d(desiredRotation)), 0, new
   // Rotation2d(desiredRotation)));
-      return Optional.of(new Rotation2d(desiredRotation + 90));
+      return Optional.of(Rotation2d.fromDegrees(desiredRotation + 90.0));
     } else {
       // return an empty optional when we don't want to override the path's rotation
       return Optional.empty();
